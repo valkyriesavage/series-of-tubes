6b649a39bd42a43583cbfd208d6196e8941e061e
Valkyrie Savage <valkyrie.savage@autodesk.com>
2014-04-08 12:02:56 -0400

    code for pen holder

---
+++ example_objects/code/pen_holder/digital_reflectance/digital_reflectance.ino
@@ -0,0 +1,84 @@
+//Code for the QRE1113 Digital board
+//Outputs via the serial terminal - Lower numbers mean more reflected
+//3000 or more means nothing was reflected.
+
+int MARKER = 2;
+int PEN = 3;
+int PENCIL = 4;
+int PAINTBRUSH = 5;
+
+int THRESH[] = {-1, -1, 300, 300, 100, 180};
+
+boolean debug = false;
+
+void setup(){
+  Serial.begin(9600); 
+}
+
+
+void loop(){
+
+  boolean mOut = absent(MARKER);
+  boolean pOut = absent(PEN);
+  boolean cOut = absent(PENCIL);
+  boolean bOut = absent(PAINTBRUSH);
+  
+  if(pOut) {
+    if(debug) Serial.println("Pen is out!");
+    else Serial.write(1);
+  }
+  if(mOut) {
+    if(debug) Serial.println("Marker is out!");
+    else Serial.write(2);
+  }
+  if(cOut) {
+    if(debug) Serial.println("Pencil is out!");
+    else Serial.write(3);
+  }
+  if(bOut) {
+    if(debug) Serial.println("Paintbrush is out!");
+    else Serial.write(4);
+  }
+  
+  if(!mOut && !pOut && !cOut && !bOut) {
+    if(debug) Serial.println("A place for every thing, and every thing in its place.\n"); 
+    else Serial.write(0);
+  }
+  /*
+  int pen = readQD(PEN);
+  int marker = readQD(MARKER);
+  int brush = readQD(PAINTBRUSH);
+  int pencil = readQD(PENCIL);
+  Serial.print("pen: ");
+  Serial.print(pen);
+  Serial.print("\tmarker: ");
+  Serial.print(marker);
+  Serial.print("\tbrush: ");
+  Serial.print(brush);
+  Serial.print("\tpencil: ");
+  Serial.println(pencil);
+  */
+}
+
+boolean absent(int pin) {
+  return readQD(pin) > THRESH[pin]; 
+}
+
+
+int readQD(int pin){
+  //Returns value from the QRE1113 
+  //Lower numbers mean more refleacive
+  //More than 3000 means nothing was reflected.
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, HIGH );  
+  delayMicroseconds(10);
+  pinMode( pin, INPUT );
+
+  long time = micros();
+
+  //time how long the input is HIGH, but quit after 3ms as nothing happens after that
+  while (digitalRead(pin) == HIGH && micros() - time < 3000); 
+  int diff = micros() - time;
+
+  return diff;
+}
---
+++ example_objects/code/pen_holder/show_state_of_pens/show_state_of_pens.pde
@@ -0,0 +1,43 @@
+// Example by Tom Igoe
+
+import processing.serial.*;
+
+Serial myPort;  // The serial port
+PFont f;
+
+void setup() {
+  size(400,400);
+  // List all the available serial ports
+  //println(Serial.list());
+  // Open the port you are using at the rate you want:
+  myPort = new Serial(this, Serial.list()[0], 9600);
+  f = createFont("Arial",64,true); // Arial, 16 point, anti-aliasing on
+}
+
+void draw() {
+  background(0);
+  String txt = "no connection :(";
+  while (myPort.available() > 0) {
+    int inByte = myPort.read();
+    switch(inByte) {
+      case 0:
+          txt = "All in!";
+          break;
+      case 1:
+          txt = "Pen out";
+          break;
+      case 2:
+          txt = "Marker out";
+          break;
+      case 3:
+          txt = "Pencil out";
+          break;
+      case 4:
+          txt = "Brush out";
+          break;
+    }
+  }
+  fill(255);
+  textFont(f);
+  text(txt,10,height/2);
+}
